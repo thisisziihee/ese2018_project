b00b0339fadb5938e86f9666adf1d24bf9d7de25
kim ji hee <jihee_0210@naver.com>
2018-11-28 23:03:30 -0800

    timer

--- timer.c
+++ timer.c
@@ -3,101 +3,108 @@
 #include<signal.h>
 #include<unistd.h>
 #include <stdlib.h>
-void reset_timer();
+void set_timer();
 void stop_timer();
 void restart_timer();
 void timer();
 int createTimer(timer_t *imerID, int sec, int msec);
 
-int stopflag=0;
+struct sigevent te;
+struct itimerspec its;
+struct sigaction sa;
 
-void timer() // Timer to be recalled according to the timer interval
+timer_t timerID;
+timer_t stoptimer;
+int leftsec;
+
+
+
+void set_timer()
 {
-  printf(" Time's UP!\n");
-  exit(0);
-}
-
-int createTimer( timer_t *timerID, int sec, int msec)
-{
-    struct sigevent         te;
-    struct itimerspec       its;
-    struct sigaction        sa;
-    int                     sigNo = SIGRTMIN;
-
-    /* Set up signal handler. */
-    sa.sa_flags = SA_SIGINFO;
-    sa.sa_sigaction = timer;     // 타이머 호출시 호출할 함수
-    sigemptyset(&sa.sa_mask);
-
-    if (sigaction(sigNo, &sa, NULL) == -1)
-    {
-        printf("sigaction error\n");
-        return -1;
-    }
-    te.sigev_notify = SIGEV_SIGNAL;
-    te.sigev_signo = sigNo;
-    te.sigev_value.sival_ptr = timerID;
-    timer_create(CLOCK_REALTIME, &te, timerID);
-
-    its.it_interval.tv_sec = sec;
-    its.it_interval.tv_nsec = msec;
-    its.it_value.tv_sec = sec;
-    its.it_value.tv_nsec = msec;
-    timer_settime(*timerID, 0, &its, NULL);
-    return 0;
-}
-
-void reset_timer()
-{
-  printf(" reset timer(unit:sec) : ");
+  printf(" 타이머 시간을 설정하세요 : ");
   int mysec;
   scanf("%d",&mysec);
-  printf(" you set the %d sec\n",mysec);
-  timer_t timerID;
-  while( stopflag != 1 )
-  {
-    createTimer(&timerID,mysec,0);
-  }
+  createTimer(&timerID,mysec,0);
+  printf(" %d 초 남았습니다.\n", timer_gettime(timerID, &its));
   return;
 }
 
 void stop_timer()
 {
-  printf(" stop timer\n");
-  return;
+  printf(" 타이머를 잠시 중단합니다.\n");
+  //현재 남은 시간 받아와서 timr_t stoptimer에 저장
+  /*if( timer_delete(timerID) == 0 )
+  {
+    printf("success\n");
+  }*/
+  //return;
 }
 
 void restart_timer()
 {
-  printf(" restart timer\n");
+  printf(" 타이머를 다시 시작합니다.\n");
+  // timer_t stoptimer 받아와서 다시 시작
+ /* while(1)
+  {
+    createTimer(&stoptimer,leftsec,0);
+  }*/
   return;
+}
+
+void timer()
+{
+  printf(" 타이머가 종료되었습니다.\n");
+  exit(0);
+}
+
+int createTimer( timer_t *timerID, int sec, int msec)
+{
+
+  int sigNo = SIGRTMIN;
+  //Handler
+  sa.sa_flags = SA_SIGINFO;
+  sa.sa_sigaction = timer;
+  sigemptyset(&sa.sa_mask);
+
+  if (sigaction(sigNo, &sa, NULL) == -1)
+  {
+    printf("sigaction error\n");
+    return -1;
+  }
+
+  te.sigev_notify = SIGEV_SIGNAL;
+  te.sigev_signo = sigNo;
+  te.sigev_value.sival_ptr = timerID;
+  timer_create(CLOCK_REALTIME, &te, timerID);
+
+  its.it_interval.tv_sec = sec;
+  its.it_interval.tv_nsec = 0;
+  its.it_value.tv_sec = sec;
+  its.it_value.tv_nsec = msec;
+
+  timer_settime(*timerID, 0, &its, NULL);
+
+  return 0;
 }
 
 int main()
 {
-  printf(" mode  < R : RESET, S : STOP, T : RESTART >\n ");
+  printf(" < R : RESET, S : STOP, T : RESTART > 원하는 모드를 선택하세요. \n ");
   int data;
-  printf("Enter the mode you want : ");
   data = getchar();
   while(1)
   {
     switch(data)
     {
       case 'R' :
-        reset_timer();
-        break;
+        set_timer(); break;
       case 'S' :
-	stopflag = 1;
-        stop_timer();
-        break;
+        stop_timer(); break;
       case 'T' :
-        restart_timer();
-        break;
-      default :
-        printf("Try Again\n");
-        break;
+        restart_timer(); break;
+      default :	break;
      }
-  break;
+    data=getchar();
   }
 }
 
