c3ccaf72e19d6be697a06aa9371e7b85ed461e6b
kim ji hee <jihee_0210@naver.com>
2018-12-03 21:50:22 -0800

    completed

--- TIMER/timer.c
+++ TIMER/timer.c
@@ -24,9 +24,11 @@ int get_time()
 {
   time(&now);
   t = localtime(&now);
-  int hour = t->tm_hour; int min = t->tm_min; int sec = t->tm_sec;
+  int hour = t->tm_hour;
+  int min = t->tm_min;
+  int sec = t->tm_sec;
   int val = min*60 + sec;
-  return Val;
+  return val;
 }
 
 void set_timer()
@@ -34,7 +36,7 @@ void set_timer()
   printf(" 타이머 시간을 설정하세요 : ");
   scanf("%d",&mysec);
   createTimer(&timerID,mysec,0);
-  system("date");
+  // system("date");
   myVal = get_time();
   return;
 }
@@ -43,12 +45,12 @@ void delete_timer()
 {
   if ( timerID == 0 )
   {
-    printf("현재 설정된 타이머가 없습니다.\n");
+    printf(" 현재 설정된 타이머가 없습니다.\n");
     return;
   }
   else
   {
-    printf("타이머를 강제로 종료합니다.\n");
+    printf(" 타이머를 강제로 종료합니다.\n");
     timer_delete(timerID);
     exit(0);
   }
@@ -57,25 +59,29 @@ void delete_timer()
 
 void stop_timer()
 {
-  printf(" 타이머를 잠시 중단합니다.\n");
-  timer_delete(timerID);
-  system("date");
-  elapse_time = get_time() - myVal;
-  time_remaining = mysec - elapse_time;
-  printf("%d left\n", time_remaining);
-  return;
+  if ( timerID == 0 ) {
+    printf(" 현재 설정된 타이머가 없습니다.\n");
+    return;
+  }
+  else
+  {
+    timer_delete(timerID);
+    elapse_time = get_time() - myVal;
+    time_remaining = mysec - elapse_time;
+    printf(" 타이머를 잠시 중단합니다. %d 초 남았습니다. \n", time_remaining);
+    return;
+  }
 }
 
 void resume_timer()
 {
 
   if ( timerID == 0 ){
-    printf("현재 대기 중인 타이머가 없습니다.\n");
+    printf(" 현재 대기 중인 타이머가 없습니다.\n");
     return;
   }
   else {
-    printf(" 타이머를 다시 시작합니다.\n");
-    system("date");
+    printf(" %d 초 타이머를 다시 시작합니다.\n",time_remaining);
     createTimer(&timerID, time_remaining, 0);
     return;
   }
@@ -111,9 +117,6 @@ int createTimer( timer_t *timerID, int sec, int msec)
   its.it_interval.tv_nsec = 0;
   its.it_value.tv_sec = sec;
   its.it_value.tv_nsec = msec;
-  printf("its.it_value.tv_sec : %d\n", its.it_value.tv_sec);
-  printf("its.it_vlaue.tv_nsec : %d\n",its.it_value.tv_nsec);
-
   timer_settime(*timerID, 0, &its, NULL);
   return 0;
 }
